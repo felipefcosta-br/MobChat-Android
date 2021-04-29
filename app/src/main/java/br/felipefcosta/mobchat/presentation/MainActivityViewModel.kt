4d2460eb4a5e1e311a335badcd412ddfca2c700a
Felipe Faria da Costa <felipefcrj@outlook.com>
2021-04-30 01:23:20 +0200

    Update 1.0

--- app/src/main/java/br/felipefcosta/mobchat/presentation/MainActivityViewModel.kt
+++ app/src/main/java/br/felipefcosta/mobchat/presentation/MainActivityViewModel.kt
@@ -36,31 +36,11 @@ class MainActivityViewModel(
 
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    fun startConnection() {
-
-        var token = authRepository.getStoragedToken()
-        var profileTemp = profileRepository.getStorageProfile()
-
-        /*if (token != null && profileTemp != null) {
-            profile = profileTemp
-            val userIdGuid = UUID.fromString(profile.id!!)
-
-            //useInsecureSSL()
-            SignalRHubService.startHubConnection(token, userIdGuid)
-            runBlocking {
-                SignalRHubService.connectToHub(profile.id!!)
-            }
-
-        }*/
-
-        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        fun finishConnection(){
-            runBlocking {
-                SignalRHubService.disconnect()
-            }
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun finishConnection(){
+        runBlocking {
+            SignalRHubService.disconnect()
         }
-
     }
 
     fun getUserProfile(success: (Profile) -> Unit, failure: () -> Unit) {
@@ -78,26 +58,5 @@ class MainActivityViewModel(
         }
 
     }
-
-    /*fun useInsecureSSL() {
-
-        // Create a trust manager that does not validate certificate chains
-        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-            override fun getAcceptedIssuers(): Array<X509Certificate>? = null
-            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) = Unit
-            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) = Unit
-        })
-
-        val sc = SSLContext.getInstance("SSL")
-        sc.init(null, trustAllCerts, java.security.SecureRandom())
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
-
-        // Create all-trusting host name verifier
-        val allHostsValid = HostnameVerifier { _, _ -> true }
-
-        // Install the all-trusting host verifier
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid)
-    }*/
-
 
 }
