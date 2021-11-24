b1a7747d88c214d1b852cfffef51a3ecf8566b8c
Felipe Faria da Costa <felipefcosta.br@outlook.com>
2021-11-24 13:07:58 +0100

    Update Constants.kt

--- app/src/main/java/br/felipefcosta/mobchat/utils/Constants.kt
+++ app/src/main/java/br/felipefcosta/mobchat/utils/Constants.kt
@@ -1,19 +1,17 @@
 package br.felipefcosta.mobchat.utils
 
 object Constants {
-    const val TOKEN_URL = "https://mobchat-iammicroservice-identity.azurewebsites.net/"
-    const val APPUSER_API_URL = "https://mobchat-iammicroservice-api.azurewebsites.net/api/"
-    const val PROFILE_API_URL = "https://mobchat-profilemicroservice-api.azurewebsites.net/api/"
+    const val TOKEN_URL = ""
+    const val APPUSER_API_URL = ""
+    const val PROFILE_API_URL = ""
     //const val CHATHUB_API_URL = "https://10.0.2.2:5001/hubs/chathub"
-    const val CHATHUB_API_URL = "https://mobchat-chatmicroservice-api.azurewebsites.net/hubs/chathub"
-    const val CHAT_API_URL = "http://mobchat-chatmicroservice-api.azurewebsites.net/api/"
-    const val BLOB_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=" +
-            "mobchatstoragecontainer;AccountKey=oBH56HdLB5s1ej3fNEB8MCPbdA4VdzPi/XaW364M6XS0PP1+" +
-            "q28uDuKFHmniReFELzKfhfzE2/5PO96kAUVztA==;EndpointSuffix=core.windows.net";
-    const val GRANT_TYPE = "password"
-    const val EMAIL = "admin"
-    const val PASSWORD = "00ProDev61#MobChat00"
-    const val CLIENT_ID = "MobChatUserMobileApp_ClientId"
+    const val CHATHUB_API_URL = ""
+    const val CHAT_API_URL = ""
+    const val BLOB_STORAGE_CONNECTION_STRING = "";
+    const val GRANT_TYPE = ""
+    const val EMAIL = ""
+    const val PASSWORD = ""
+    const val CLIENT_ID = ""
 
 }
 
